e18cee994293bbc06bab859570de9fabba12995f
Giribabu1492 <jinagagiribabu@gmail.com>
2026-06-03 21:05:19 +0530

    my messgae

---
+++ 60-acm/parameters.tf
@@ -0,0 +1,7 @@
+resource "aws_ssm_parameter" "web_alb_certificate_arn" {
+  name      = "/${var.project_name}/${var.environment}/web_alb_certificate_arn"
+  type      = "String"
+  value     = aws_acm_certificate.expense.arn
+  overwrite = true
+
+}
