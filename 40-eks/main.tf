746b8e2e2300aec86b98d706a59fcfc79be04ee8
Giribabu1492 <jinagagiribabu@gmail.com>
2026-06-04 16:29:34 +0530

    my messgae

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -60,11 +60,11 @@ module "eks" {
     green = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       #ami_type       = "AL2_x86_64"
-      instance_types = ["m5.xlarge"]
+      instance_types = ["t3.micro"]
       key_name       = aws_key_pair.eks.key_name
 
       min_size     = 2
-      max_size     = 10
+      max_size     = 6
       desired_size = 2
       iam_role_additional_policies = {
         AmazonEBSCSIDriverPolicy     = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
